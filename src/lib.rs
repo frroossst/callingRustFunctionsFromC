64bd76b801c48f9aa16cf83a4be19072383b1271
home <adhyanpatel@gmail.com>
2022-08-16 13:08:07 -0600

    uploaded code to Teensy via PlatformIO

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,20 @@
 //extern crate panic_halt;
 
 #[no_mangle]
-pub unsafe extern "C" fn rust_function() -> u8
+pub extern "C" fn rust_function() -> u8
     {
     0
     //println!("Hello World! from Rust");
     }
 
 #[no_mangle]
-pub unsafe extern "C" fn rust_giveback_u8(a: u8) -> u8
+pub extern "C" fn rust_giveback_u8(a: u8) -> u8
     {
     a
     }
 
 #[no_mangle]
-pub unsafe extern "C" fn rust_pfunction() -> u8
+pub extern "C" fn rust_pfunction() -> u8
     {
     0
     //println!("new lib")
